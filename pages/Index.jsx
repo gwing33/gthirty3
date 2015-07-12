6b42c3337779263c1f580fe6a8e6cf45d49df4da
Gerald Leenerts <gerald.leenerts@gmail.com>
2015-07-11 23:35:06 -0600

    reworking the entire application.

---
+++ pages/Index.jsx
@@ -0,0 +1,12 @@
+'use strict';
+import React from 'react';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>Hello World!</div>
+    );
+  }
+}
+
+module.exports = Index;
