b3dd631e7b147c6269efcd6c8ab17d7a23586692
Gerald Leenerts <gerald.leenerts@gmail.com>
2015-07-19 17:23:28 -0600

    Minor Style fix for non standard lists

--- pages/Index.jsx
+++ pages/Index.jsx
@@ -31,14 +31,14 @@ class Index extends React.Component {
         <h2 styles={styles.h2}>EXPERIENCES</h2>
         <hr />
         <h5 styles={styles.h5}>Pearl Coffee <em>(June 2013 - Present)</em></h5>
-        <ul>
+        <ul className='list'>
           <li>Co-Founder, Lead Branding and Marketing Strategist</li>
           <li>Designed the full experience of the Pearl Coffee brand.</li>
           <li>Focus on eductation as a large part of our marketing strategy.</li>
         </ul>
 
         <h5 styles={styles.h5}>Life Time Fitness <em>(April 2014 - Present)</em></h5>
-        <ul>
+        <ul className='list'>
           <li>Senior UX Developer / Senior Front-end Engineer / Senior Software Engineer</li>
           <li>React / Flux lead engineer.</li>
           <li>Functional Programming and Streaming system architectures.</li>
@@ -46,7 +46,7 @@ class Index extends React.Component {
         </ul>
 
         <h5 styles={styles.h5}>SpyFu <em>(May 2011 - April 2014)</em></h5>
-        <ul>
+        <ul className='list'>
           <li>Helped redesign the entire front-end of SpyFu.com, had to work around the large data aspect.</li>
           <li>Set up processes such as a reliable CI / CD system for consisten deployments.</li>
           <li>Was a leader in helping direct the company with their vision.</li>
@@ -55,7 +55,7 @@ class Index extends React.Component {
 
 
         <h5 styles={styles.h5}>Universal UClick <em>(July 2010 - May 2011)</em></h5>
-        <ul>
+        <ul className='list'>
           <li>Worked with the auther of Doonsebury to create their website, helping them understand the difference between print and web design.</li>
           <li>Implemented large Ruby on Rails web apps.</li>
           <li>Designed database structures and software system to handle large file aggregation and to help optimize for both web and print solutions.</li>
@@ -64,7 +64,7 @@ class Index extends React.Component {
 
         <h2 styles={styles.h2}>EDUCATION</h2>
         <h5 styles={styles.h5}>Bachelors of Fine Arts with an emphasis in Computer Animation</h5>
-        <ul>
+        <ul className='list'>
           <li>Missouri State University</li>
         </ul>
 
@@ -72,7 +72,7 @@ class Index extends React.Component {
         <h2 styles={styles.h2}>SKILLS & READS</h2>
         <hr />
         <h5 styles={styles.h5}>Design & Product</h5>
-        <ul>
+        <ul className='list'>
           <li>UXPin and Hand Drawn Wireframes</li>
           <li>Adobe Creative Suite: Photoshop, Illustrator, After Effects</li>
           <li>Sketch 3</li>
@@ -81,13 +81,13 @@ class Index extends React.Component {
         </ul>
 
         <h5 styles={styles.h5}>Methodologies</h5>
-        <ul>
+        <ul className='list'>
           <li>The Hacker Way</li>
           <li>Agile / Scrum</li>
         </ul>
 
         <h5 styles={styles.h5}>Development & Engineering</h5>
-        <ul>
+        <ul className='list'>
           <li>JavaScript: React, Flux, BackboneJS, MV* Patterns</li>
           <li>LESS / SASS / CSS / HTML</li>
           <li>Scala, NodeJS, .NET, Ruby, Python, PHP and Java</li>
@@ -97,7 +97,7 @@ class Index extends React.Component {
         </ul>
 
         <h5 styles={styles.h5}>Favorite Books</h5>
-        <ul>
+        <ul className='list'>
           <li>Don't make me Think</li>
           <li>Design of Everyday Things</li>
           <li>Emotional Design</li>
