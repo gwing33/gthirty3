63171cf853415ad69e32e1ad27abfec6287d539f
Gerald Leenerts <gerald.leenerts@gmail.com>
2015-07-22 22:03:05 -0600

    I guess I had index instead of Index.

--- routes/routes.jsx
+++ routes/routes.jsx
@@ -3,7 +3,7 @@ import Router from 'react-router';
 import { Route, DefaultRoute } from 'react-router';
 
 import Base from '../pages/base.jsx';
-import Index from '../pages/index.jsx';
+import Index from '../pages/Index.jsx';
 
 let Routes = (
   <Route path='/?' name='app' handler={Base}>
